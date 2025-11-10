ead43e33f9de14d27001035d36d3852798bff3da
aymene69 <aymene69@github.com>
2025-11-10 16:20:33 +0100

    Ajout blog

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/', title: 'Accueil' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/about', title: 'À propos' },
+]
+
+export default headerNavLinks
